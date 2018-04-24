7a7a1d9577c69d07b3d2c114d3f0110c93b5c8d9
Jonas Smith <pictofoster@gmail.com>
2018-04-24 14:09:42 -0500

    adding blur affect to main window.

--- SpotifyAlarm/SpotifyAlarm/MainWindow.xaml.cs
+++ SpotifyAlarm/SpotifyAlarm/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
@@ -18,6 +20,36 @@ using System.Windows.Threading;
 
 namespace SpotifyAlarm
 {
+  internal enum AccentState
+  {
+    ACCENT_DISABLED = 0,
+    ACCENT_ENABLE_GRADIENT = 1,
+    ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+    ACCENT_ENABLE_BLURBEHIND = 3,
+    ACCENT_INVALID_STATE = 4
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  internal struct AccentPolicy
+  {
+    public AccentState AccentState;
+    public int AccentFlags;
+    public int GradientColor;
+    public int AnimationId;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  internal struct WindowCompositionAttributeData
+  {
+    public WindowCompositionAttribute Attribute;
+    public IntPtr Data;
+    public int SizeOfData;
+  }
+
+  internal enum WindowCompositionAttribute
+  { WCA_ACCENT_POLICY = 19 }
+
+
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
@@ -25,9 +57,41 @@ namespace SpotifyAlarm
   {
     public DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
 
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      EnableBlur();
+    }
+
+    internal void EnableBlur()
+    {
+      var windowHelper = new WindowInteropHelper(this);
+
+      var accent = new AccentPolicy();
+      accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
+
+      var accentStructSize = Marshal.SizeOf(accent);
+
+      var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+      Marshal.StructureToPtr(accent, accentPtr, false);
+
+      var data = new WindowCompositionAttributeData();
+      data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+      data.SizeOfData = accentStructSize;
+      data.Data = accentPtr;
+
+      SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+      Marshal.FreeHGlobal(accentPtr);
+    }
+
     public MainWindow()
     {
       InitializeComponent();
+       EnableBlur();
       StartTimers();
       LoadThemes();
     }
@@ -66,6 +130,11 @@ namespace SpotifyAlarm
     private void Grid_MouseDown_1(object sender, MouseButtonEventArgs e)
     {
       System.Windows.Application.Current.Shutdown();
+    }
+
+    private void Window_Loaded_1(object sender, RoutedEventArgs e)
+    {
+      EnableBlur();
     }
   }
 }
