03bedd69f380a130d80c2f72aa01cfa26f0d4554
Jonas Smith <pictofoster@gmail.com>
2018-05-15 12:15:21 -0500

    adding custom spotifyAlarm icon

--- SpotifyAlarm/SpotifyAlarm/obj/Debug/SpotifyAlarm_Content.g.i.cs
+++ SpotifyAlarm/SpotifyAlarm/obj/Debug/SpotifyAlarm_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/plus.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("spotifyalarm.ico")]
 
 
