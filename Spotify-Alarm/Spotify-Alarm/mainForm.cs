2175c6ed452b93f420ba04120386d4ea56f2ce9f
Jonas Smith <pictofoster@gamil.com>
2019-07-20 19:10:09 -0500

    Adding more documentation

--- Spotify-Alarm/Spotify-Alarm/mainForm.cs
+++ Spotify-Alarm/Spotify-Alarm/mainForm.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿/// Programmer : Jonas Smith
+/// Purpose    : This is a newer form for the spotify-Alarm application I have created.
+/// 
+
+
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace Spotify_Alarm
 {
